e173f97c3e19e6533e59c3e60071d3ac299c75a6
Mingming Fan <Mingming Fan>
2017-06-29 11:28:07 -0400

    calibration for a webcamera and a leap motion

---
+++ leapmotionwebcamera-calibration.py
@@ -0,0 +1,136 @@
+import numpy as np
+import cv2
+import sys
+import Leap, sys, thread, time
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+
+
+class SampleListener(Leap.Listener):
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+    bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
+    state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+
+    def on_init(self, controller):
+        print "initialized"
+
+    def on_connect(self, controller):
+        print "leap motion connected"
+        #controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE)
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP)
+        #controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP)
+        #controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
+
+        controller.config.set("Gesture.KeyTap.MinDownVelocity", 5.0)
+        controller.config.set("Gesture.KeyTap.HistorySeconds", .3)
+        controller.config.set("Gesture.KeyTap.MinDistance", 0.5)
+        controller.config.save()
+
+        controller.set_policy_flags(Leap.Controller.POLICY_BACKGROUND_FRAMES)
+
+    def on_disconnect(self, controller):
+        # Note: not dispatched when running in a debugger.
+        print "leap motion disconnected"
+
+    def on_exit(self, controller):
+        print "exited"
+
+    def on_frame(self, controller):
+        self.frame = controller.frame()
+
+def to_np(v):
+    return np.float32([v[0], v[1], v[2]])
+
+def main():
+    controller = Leap.Controller()
+    controller.set_policy_flags(Leap.Controller.POLICY_BACKGROUND_FRAMES)
+
+    # Initial Intrinsic Camaera Parameters
+    # the resolution is 1280x720,
+    cm0 = np.float32([[  1.25542912e+03,   0.00000000e+00,   6.39500000e+02],
+           [  0.00000000e+00,   1.23283056e+03,   3.59500000e+02],
+           [  0.00000000e+00,   0.00000000e+00,   1.00000000e+00]])
+    dist0 = np.float32([[ 0.54520465, -1.47166739,  0.02451086, -0.02110954,  1.96975927]])
+
+
+    cm=None
+    #test points are 5 * 3
+    #test_pos=[(0.1+(1.0*i/4)*0.8,0.1+(1.0*j/2)*0.8) for i in range(5) for j in range(3)]
+
+    #test points are 5 * 5
+    test_pos=[(0.1+(1.0*i/4)*0.8,0.1+(1.0*j/4)*0.8) for i in range(5) for j in range(5)]
+
+    result = []
+    screen_pos = []
+
+    # opencv related content
+    cv2.startWindowThread()
+    cap = cv2.VideoCapture(3)
+    #ret = cap.set(3,1280);
+    #ret = cap.set(4,720);
+    ret = cap.set(3,960);
+    ret = cap.set(4,720);
+
+    idx = 0
+
+    while(True):
+        ret, img = cap.read()
+        H, W = img.shape[:2]
+        keycode = cv2.waitKey(1) & 0xff
+        frame = controller.frame()
+        tip = None
+
+        if len(frame.hands) > 0:
+            hand1 = frame.hands[0]
+            fingers = hand1.fingers.finger_type(1)
+            if len(fingers):
+                #use first fingertip's position for calibration, so make sure only show one fingertip during calibration
+                tip = fingers[0].tip_position
+        else:
+            pass
+
+        if keycode == ord('q'):
+            break;
+        elif keycode == ord(' '):
+            if tip:
+                screen_pos.append((int(test_pos[idx][0]*W), int(test_pos[idx][1]*H)))
+
+                result.append((tip[0], tip[1], tip[2]))
+                idx += 1
+                if idx >= len(test_pos):
+                    idx = 0
+        cv2.circle(img, (int(test_pos[idx][0]*W), int(test_pos[idx][1]*H)), 10, (0,0,255) if tip else (255,0,0), -1)
+        img = cv2.flip(img, 1)
+        cv2.imshow('frame', img)
+
+    cv2.destroyWindow('frame')
+    cap.release()
+    controller.clear_policy(Leap.Controller.POLICY_BACKGROUND_FRAMES)
+
+
+    screen_pos2 = [(W-1-x, y) for (x,y) in screen_pos]
+
+    retval, cm, dist, rvec, tvec = cv2.calibrateCamera(np.float32([result]), \
+                                                   np.float32([screen_pos]), (W, H), cm0.copy(), dist0.copy(),\
+                                                   flags=cv2.CALIB_USE_INTRINSIC_GUESS)
+
+    np.set_printoptions(threshold='nan')
+    np.set_printoptions(suppress = True)
+
+    print retval, rvec, tvec, cm, dist
+
+    controller.clear_policy(Leap.Controller.POLICY_BACKGROUND_FRAMES)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Remove the sample listener when done
+        #controller.remove_listener(listener)
+        pass
+
+if __name__ == "__main__":
+    main()
